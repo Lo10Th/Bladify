3d1c368e4b4f8cc71dadb5f60b5c832732cee154
Lo10Th <karl.cornelius100@gmail.com>
2023-09-11 18:40:40 +0200

    the start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    fetch('http://localhost:5000')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  }, []);
+
+  return (
+    <h1>Hi Mom</h1>
+  );
+}
+
+export default App;
