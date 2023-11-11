b2c2ee6f3fab65e04a7bf1db886d7947c4d39d6a
Lo10Th <karl.cornelius100@gmail.com>
2023-11-11 20:05:39 +0100

    first step to create mobile app

---
+++ mobile-app/components/searchbar/searchresultlist.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { SearchResult } from "./searchresult";
+
+import "./searchresultlist.css";
+
+export const SearchResultsList = ({ results }) => {
+  return (
+    <div className="results-list">
+      {results && results.length > 0 ? (
+        <ul>
+          {results.map((result) => (
+            <SearchResult result={result} key={result.id} />
+          ))}
+        </ul>
+      ) : (
+        <p>No results found.</p>
+      )}
+    </div>
+  );
+};
